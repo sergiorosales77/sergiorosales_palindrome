74cdccc183f46817a160b282f58b561b40f4478f
sergiorosales77 <sergio.rosales@gmail.com>
2022-11-06 19:39:35 -0600

    Initialize repository

---
+++ lib/sergiorosales_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sergiorosales_palindrome/version"
+
+module SergiorosalesPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
